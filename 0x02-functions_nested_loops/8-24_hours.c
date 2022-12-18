9c8bdbb7fb68fcf3c8d42a48bae4f003a40a033e
Derullah <rogatewady98@gmail.com>
2022-12-17 23:28:54 -0800

    commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *
+ * a function that prints evry minute of the day of jack bauer from 0:00 to 23:59
+ *
+ * writen by derullah rogat
+ * Return: 0 .
+ */
+
+int main(void)
+{
+
+	jack_bauer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+ 
