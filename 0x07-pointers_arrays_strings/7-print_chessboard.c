3808ba9fca8b42142268ddc06709a6906b79e4f6
Derullah <rogatewady98@gmail.com>
2023-01-04 14:08:23 -0800

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *  main - check the code
- *  Return: Always 0.
+ * print_chessboard - prints the chess board
+ * @a: pointer
+ * Return: no return
  */
 
-int main(void)
+void print_chessboard(char (*a)[8])
+
 {
-	char board[8][8] = {
-		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-	};
-	print_chessboard(board);
-	return (0);
+
+
+
+	unsigned int i, m =0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\0');	
+		}
+		_putchar(a[i/8][i - m]);
+	}
+	_putchar('\0');
+
 }
