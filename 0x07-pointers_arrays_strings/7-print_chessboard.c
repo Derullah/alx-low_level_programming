7a34cd502c67481e0ffd61cd8951e4d52d2508c8
Derullah <rogatewady98@gmail.com>
2023-01-04 14:12:02 -0800

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,16 +12,16 @@ void print_chessboard(char (*a)[8])
 
 
 
-	unsigned int i, m =0;
+	unsigned int i, m = 0;
 
 	for (i = 0; i < 64; i++)
 	{
 		if (i % 8 == 0 && i != 0)
 		{
 			m = i;
-			_putchar('\0');	
+			_putchar('\0');
 		}
-		_putchar(a[i/8][i - m]);
+		_putchar(a[i / 8][i - m]);
 	}
 	_putchar('\0');
 
